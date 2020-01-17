a7915f7946e23d970479cec5f9a5829ffa85aea7
mangoALCATRAZ <angeluccimatt4@gmail.com>
2020-01-16 23:45:24 -0500

    Added more sophisticaed drop down functionality based on Prof. Sally K's implementation. This is more extensible and allows for easy addition and subtraction of navbar menu element without having to mess around with any additional JS code.

--- web/js/dropDown.js
+++ web/js/dropDown.js
@@ -3,15 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-function dropDown(elementId){
-    var ele = document.getElementById(elementId);
+function dropInit(dropClassName){
+    var dropList = document.getElementsByClassName(dropClassName);
     
-    if(ele.style.visibility === "hidden"){
-        ele.style.visibility = "visible";
+    for(var i = 0; i < dropList.length; i++){
+        dropList[i].style.visibility = "hidden";
     }
-    else{
-        ele.style.visibility = "hidden";
+}
+
+function dropDown(headerClassName, dropClassName){
+    var headList = document.getElementsByClassName(headerClassName);
+    
+    for(var i = 0; i < headList.length; i++){
+        headList[i].onclick = function (){
+            var dropContent = this.getElementsByClassName(dropClassName)[0];
+            
+            toggle(dropContent);
+        };
     }
+    
+    function toggle(elem){
+        if(elem.style.visibility === "hidden"){
+            elem.style.visibility = "visible";
+        }
+        else if(elem.style.visibility === "visible"){
+            elem.style.visibility = "hidden";
+        }
+    }
+    
+    function hideExcept(loneElem){
+        var dropList = document.getElementsByClassName(dropClassName);
+        for(var i = 0; i < dropList.length; i++){
+            if(loneElem === null || !(dropList[i] === loneElem)){
+                if(dropList[i].style.visibility === "visible"){
+                    dropList[i].style.visibility = "hidden";
+                }
+            }
+        }
+    }
+    
+    window.onclick = function(event){
+        if(!(event.target.className.includes(headerClassName))){
+            hideExcept(null);
+        }
+    };
 }
